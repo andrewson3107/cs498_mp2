b0e09449208791368404a847000876155c26125f
Andrew <andrewson0803@gmail.com>
2024-01-28 18:15:51 -0600

    Add files via upload.

---
+++ serve.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import socket, subprocess
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST', 'GET'])
+def serve():
+    if request.method == 'GET':
+        return socket.gethostname()
+    elif request.method == "POST":
+        subprocess.Popen(['python3', 'stress_cpu.py']) 
+        
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=5000)
